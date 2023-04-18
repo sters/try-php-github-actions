338c94e9e362f7f700df7ba14ccbe0f1ce5cb8c6
sters <sters@users.noreply.github.com>
2023-04-19 00:08:18 +0900

    up

---
+++ src/Traverser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ClassGraph;
+
+use PhpParser\Error;
+use PhpParser\NodeTraverser;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+
+// copy from sters/php-class-graph
+/**
+ * Traverser is wrapper of PhpParser->parse and PhpParser->traverse
+ */
+class Traverser
+{
+    /** @var Parser */
+    protected $parser;
+
+    /** @var \Closure[] function(Node $node) */
+    protected $hooks = [];
+
+    public function __construct()
+    {
+        $this->parser = (new ParserFactory)->
+            create(ParserFactory::PREFER_PHP7);
+    }
+
+    /**
+     * do traverse
+     *
+     * @param string $sourceFile
+     * @return Visitor visited file information
+     */
+    public function traverse(string $sourceFile): Visitor
+    {
+        $visitor = new Visitor;
+        foreach($this->hooks as $hook) {
+            $visitor->addHook($hook);
+        }
+
+        try {
+            $ast = $this->parser->parse(file_get_contents($sourceFile));
+        } catch (Error $error) {
+            return $visitor;
+        }
+
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor($visitor);
+        $traverser->traverse($ast);
+
+        return $visitor;
+    }
+
+    /**
+     * @param $f \Closure function(Node $node)
+     */
+    public function addHook(\Closure $f)
+    {
+        $this->hooks[] = $f;
+    }
+}
